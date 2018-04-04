1e6d4d32daf1dfcfc56c4ac9b588b60119a26e90
ligain <khokhlov86@gmail.com>
2018-04-05 00:49:07 +0300

    re-written README basic functionality

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-# Frequency Analysis of Words
 
-[TODO. There will be project description]
+# Frequency Analysis of Words  
+  
+This script could be helpful to you to find the most frequent words in a text file.
 
-# Project Goals
+## Usage from code
+```python
+>>> from lang_frequency import get_most_frequent_words
+>>> get_most_frequent_words('Your long text string ...', top=5)
+[('the', 71755), ('of', 39634), ('and', 36549), ('to', 27965), ('a', 19811)]
+```
 
+## Usage from bash
+```bash
+$ python3 lang_frequency.py -f path/to/big_text_file.txt
+Top 10 most common words in the file:
+
+Word       | Occurrences
+-----------------------
+the        |      71755
+of         |      39634
+and        |      36549
+to         |      27965
+a          |      19811
+in         |      19691
+that       |      11437
+was        |      11238
+he         |       9599
+his        |       9575
+
+```
+  
+# Project Goals  
+  
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,58 @@
+import argparse
+import re
+
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as text_file:
+        content = text_file.read()
+    return content
 
 
-def get_most_frequent_words(text):
-    pass
+def tokenize_text(text_string, speed='fast'):
+    if speed == 'fast':
+        return text_string.split()
+    elif speed == 'slow':
+        return re.split(r'[;,\s]\s*', text_string)
+
+
+def get_most_frequent_words(text_string, top=10):
+    words_list = tokenize_text(text_string, speed='slow')
+    counter = Counter(words_list)
+    return counter.most_common(top)
+
+
+def print_top_words(words_list):
+    row_format = '{:<10} | {:>10}'
+
+    print('Top {} most common words '
+          'in the file:\n'.format(len(words_list)))
+    print(row_format.format('Word', 'Occurrences'))
+    print('-' * 23)
+    for word, occurrences in words_list:
+        print(row_format.format(word, occurrences))
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Word frequency analyzer'
+    )
+
+    parser.add_argument(
+        '-f', '--filepath',
+        help='path to txt file',
+        required=True
+    )
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+
+    args = get_args()
+
+    loaded_text = load_data(args.filepath)
+
+    top_words = get_most_frequent_words(loaded_text)
+
+    print_top_words(top_words)
